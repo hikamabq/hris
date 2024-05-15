2b1301ed8d64bfa7c4111273c03fa9165144980f
vcto <victoa.rpl@gmail.com>
2024-05-15 16:04:50 +0700

    jadwal

--- resources/views/jadwal/index.blade.php
+++ resources/views/jadwal/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-white leading-tight">
-            {{ __('Jadwal') }}
+            {{ __('Jadwals') }}
         </h2>
     </x-slot>
 
